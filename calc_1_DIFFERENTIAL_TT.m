5ff1a46aaa819bce390f63db1a1863692b801a1f
Zach Eilon <1310079+eilonzach@users.noreply.github.com>
2024-08-01 12:58:16 -0700

    Update calc_1_DIFFERENTIAL_TT.m

--- calc_1_DIFFERENTIAL_TT.m
+++ calc_1_DIFFERENTIAL_TT.m
@@ -11,9 +11,11 @@
 % Commands:
 %       x   select window (beginning and end)
 %       f	change filter
+%       s   toggle display of snr
 %       y	toggle between gcarc distance and simply station order
 %       d	delete station with trace beneath cursor
 %       u	flip sign of station with trace beneath cursor
+%       c	put on cheat-sheet of phases
 %    left   prev event
 %   right 	next event 
 %      up 	increase amplitudes
@@ -32,7 +34,7 @@ dbdir = '~/Dropbox/Work/FRES_PILOT/'; % include final slash
 
 
 %% parameters
-phase = 'SKS';
+phase = 'SKS'; % available are 'P,PP,S,PKP,PKS,SKS';
 component = 'R'; %'Z', 'R', or 'T'
 resamprate = 10 ; % new, common sample rate
 % Do filtfs [12 1] for S, and [5 1] for P
@@ -41,7 +43,8 @@ filtfs = 1./[12 1]; % S [flo fhi] = 1./[Tmax Tmin] in sec
 taperx = 0.2;
 npoles = 2;
 datwind = [-200 200]; % window of data in eqar structure
-prelimwind = [-40 40];
+prelimwind = [-50 40]; % usually [-40 40], but may need to expand for longer periods
+conflimwind = [-20 30]; % usually [-10 3y0], but may need to expand for longer periods
 % prelimwind = [-25 15];
 xcorlagmax = 20;
 gcdistmax = 12;
@@ -50,10 +53,33 @@ maxgcarc = 130;
 acormin = 0.75;
 overwrite = false;
 minNtrace = 4;
+
+% parameters for the multitaper spectrogram of the phases
+WinSize = 20;
+FreqResolution=1/10;
+StepSize= 0.25;   
+% parameters for the multi-freq analysis of the phases
+Tlos = [12 9   8  8  8  7 2 1 2 1.6666666 1 ];
+This = [50 45 40 30 25 20 5 5 4 3.3333333 2]; % must be same length as above
+% phase names for the cheat sheet (predicted arrival times
+cheatsheetphases = {'P','S','PP','SKS','PKS','SKKS'};
+
+%% Progress notes
 % FRESPILOT -  S(T) got to 1863. P(Z) got to 1920. SKS(R) got to 1847
 % EARdb (I think outdated) SKS got to 1044; S got to 1287; % need to do 160-335 again on all
 firstev = 1847; 
 
+%% EVT limits
+firstev = 1; 
+% time bounds for events to consider - ignore before startdate, or after enddate
+startdate = '2014-03-01'; % format 'YYYY-MM-DD' % FOR EARdb new stations
+% startdate = '1000-01-01'; % format 'YYYY-MM-DD' % for all
+enddate   = '2025-01-01'; % format 'YYYY-MM-DD'
+% magnitude bounds for events to consider - INCLUDE the limiting values
+minmag = 6.2; % in Mw, set to 5 for all
+maxmag = 7.2; % in Mw, set to 9 for all
+% NOTE!! Also distance bounds for every phase, defined in the 
+% "GRAB DATA IN RIGHT FORMAT" section below
 
 % if you don't like any stations in particular, list them here to ignore
 % their data in the record section and xcorr
@@ -85,18 +111,30 @@ load([infodir,'/events'],'evinfo');
 % station details
 load([infodir,'/stations'],'stainfo');
 
+% some default parameters
+SNRdisp = false;
+phasedisp = false;
 ie = firstev; % up to 364
 ied = 1; %incrementer - leave as 1
 while 1 %evinfo.norids % loop on orids 
 %     if  mags(ie)<6.9, continue, end
     tic
     close all, clear('x1','x2');
+    if ie > evinfo.norids, error('ALL DONE!\n'); end
     orid = evinfo.orids(ie);
     fprintf('\n Orid %.0f %s \n\n',orid,evinfo.evtimes_IRISstr{ie}(1:end-4))
     % name files and directories
     evdir = [num2str(orid,'%03d'),'_',evinfo.datestamp(ie,:),'/'];
     datinfofile = [data_eqar_dir,evdir,'_datinfo_',phase];
     arfile      = [data_eqar_dir,evdir,'_EQAR_',phase,'_',component];
+
+    % ignore outside date or magnitude bounds
+    if  evinfo.evtimes(ie) < datenum(startdate) || evinfo.evtimes(ie) > datenum(enddate) ...
+     || evinfo.evmags(ie) < minmag || evinfo.evmags(ie) > maxmag
+        ie = ie + ied;
+        nextev = 1;        
+        continue; 
+    end
    
     % check files exist
     if ~exist([datinfofile,'.mat'],'file'), fprintf('No station mat files for this event\n'); ie = ie+ied; continue, end
@@ -118,6 +156,18 @@ while 1 %evinfo.norids % loop on orids
     end
     if all([eqar.pred_arrT]==0), fprintf('No %s arrivals for this event\n',phase), ie = ie + ied; continue, end
     
+    % account for different sample rate in eqar compared to resamprate
+    if unique([eqar.samprate])~=resamprate
+        fprintf('!! EQAR samprate is %.0f, whereas asking for resamprate = %.0f\n',unique([eqar.samprate]),resamprate)
+        ynsr = input('Would you like to switch to EQAR samprate? ','s');
+        if any(strcmp(ynsr,{'y','Y','yes'})) 
+            fprintf('Okay, resamprate now %.0f\n',unique([eqar.samprate]))
+            resamprate = unique([eqar.samprate]);
+        else
+            ie = ie + ied; continue, 
+        end
+    end
+
 %     % skip if outside appropriate distance range for the phase
 %     if strcmp(phase,'P') && mean([eqar.gcarc])>92, ie = ie + ied; continue, end
 %     if strcmp(phase,'S') && mean([eqar.gcarc])>95, ie = ie + ied; continue, end
@@ -129,6 +179,7 @@ while 1 %evinfo.norids % loop on orids
     nstas = length(datinfo);
     wlen = diff(prelimwind)*resamprate*(1+4*taperx); % window length, accounting for taper+padding
     all_dat0  = zeros(resamprate*diff(datwind),nstas);
+    dT_exist = nan(nstas,1);
     
     % LOOP ON STAS
     for is = 1:nstas
@@ -137,14 +188,23 @@ while 1 %evinfo.norids % loop on orids
         
         % skip if outside appropriate distance range for the phase        
         switch phase
-            case 'P',   if eqar(is).gcarc > 95 || eqar(is).gcarc<30, continue; end
-            case 'S',   if eqar(is).gcarc > 84 || eqar(is).gcarc<30, continue; end
-            case 'SKS', if eqar(is).gcarc > 132 || eqar(is).gcarc<86, continue; end
-            case 'PKS',   if eqar(is).gcarc > 140 || eqar(is).gcarc<128, continue; end
+            case 'P',   if eqar(is).gcarc > 99  || eqar(is).gcarc<30, fprintf('bad distance\n'),continue; end
+            case 'PP',  if eqar(is).gcarc > 170 || eqar(is).gcarc<60, fprintf('bad distance\n'),continue; end
+            case 'S',   if eqar(is).gcarc > 84  || eqar(is).gcarc<30, fprintf('bad distance\n'),continue; end
+            case 'SKS', if eqar(is).gcarc > 132 || eqar(is).gcarc<88, fprintf('bad distance\n'),continue; end
+            case 'SKKS',if eqar(is).gcarc > 132 || eqar(is).gcarc<91, fprintf('bad distance\n'),continue; end
+            case 'PKS', if eqar(is).gcarc > 140 || eqar(is).gcarc<128, fprintf('bad distance\n'),continue; end
         end
         
         % GRAB DESIRED COMPONENT
         all_dat0(:,is) = eqar(is).(['dat',component])(:);
+
+        % GRAB existing pick, if available
+        if isfield(eqar,'abs_arrT')
+            dT_exist(is) = eqar(is).abs_arrT;
+        else
+            dT_exist(is) = nan;
+        end
         
         fprintf('got data\n')
     end % loop on stas
@@ -162,27 +222,39 @@ while 1 %evinfo.norids % loop on orids
     indgd(isnan(mean(abs(all_datwf(:,indgd))))) = []; % kill nan traces
     for ik = 1:length(manualkillstas),indgd(strcmp({eqar(indgd).sta},manualkillstas{ik})) = []; end % kill bad stas traces
     
-    if strcmp(phase,'SKS') ,indgd([eqar(indgd).gcarc]<86) = []; end % kill close traces
-    
+    %I think redundant after the above.... kill certain phases if likely to overlap with others
+    if strcmp(phase,'SKKS') ,indgd([eqar(indgd).gcarc]<90) = []; end % kill close traces
+    if strcmp(phase,'SKS') ,indgd([eqar(indgd).gcarc]<88) = []; end % kill close traces
+    if strcmp(phase,'S') ,indgd([eqar(indgd).gcarc]>84) = []; end % kill far S traces
+
+    % Decide on whether EQ is worth pursuing, with enough good traces
     if length(indgd) < minNtrace, fprintf('NOT ENOUGH GOOD TRACES/ARRIVALS, skip...\n'); ie = ie + ied; continue, end
     
     fprintf('Orid %.0f %s \n',orid,evinfo.evtimes_IRISstr{ie}(1:end-4))
-	%% tauptime to help picking
-    tauptime('d',mean([eqar.gcarc]),'z',evinfo.edeps(ie))
+	
+    %% tauptime to help picking
+    phasedb = mkcheatsheet(cheatsheetphases,[eqar(indgd).gcarc],evinfo.edeps(ie));
+    % take off times of current phase to make differential times
+    phasedb.dphasetimes = phasedb.phasetimes - phasedb.phasetimes(:,strcmp(phasedb.phases,phase))*ones(1,phasedb.Nph);
   
 %% ------------------ PICK XCOR WINDOW, RECLEAN ------------------
     figxc = figure(53); clf(figxc); set(figxc,'position',[710 1 1626 1344])
     
-    %% PICK TRAVEL TIMES
+%% ------------------ PICK TRAVEL TIMES ------------------
+
+    % some default parameters
     cont=0;
     nextev=0;
     ydopt = 1;
     ampf = 1;
+    autosnwin = 1;
+
     while nextev==0
     while cont==0
-    
-        try delete(ax1); end
 
+        %% Clear and set up the axes for the plotting
+        try delete(ax1); end
+        set(figxc,'position',[710 1 1626 1344])
 
         normf = ampf*max(max(abs(all_datwf(:,indgd))));
         % y option - distance or order
@@ -192,13 +264,20 @@ while 1 %evinfo.norids % loop on orids
             yds = 1:length(indgd);
         end
         yext = [min(yds),max(yds)];
+        
+        %make axes for plotting
         ax1 = axes(figxc,'pos',[0.05 0.07 0.3 0.85]); hold on
         cols = {'b','k'};
         for ig = 1:length(indgd)
             is = indgd(ig);
-            plot(ax1,ttws,all_datwf(:,is)/normf/(10/diff(yext)) + yds(ig),'-','LineWidth',0.6,'color',cols{isodd(ig)+1})
+            colr = cols{isodd(ig)+1};
+            htr(ig) = plot(ax1,ttws,all_datwf(:,is)/normf/(10/diff(yext)) + yds(ig),'-','LineWidth',0.8,'color',colr);
             text(ax1,prelimwind(2)+1,yds(ig),datinfo(is).sta,'fontweight','bold','fontsize',20)
+
+            % add previous picks if available
+            plot(ax1,(dT_exist(is) - eqar(is).pred_arrT)*spd*[1;1],(diff(yext)/50)*[-1,1] + yds(ig),'-','LineWidth',1.5,'color',[1 0 0 0.5])
         end
+
         title(ax1,sprintf('PICK XCOR WINDOW\nORID %u, seaz %.0f, gcarc %.0f\ndep %.0fkm, Ms %.1f',...
             orid,mean([eqar.seaz]),mean([eqar.gcarc]),evinfo.edeps(ie),evinfo.evmags(ie)),'Fontsize',18)
         axis(ax1,[prelimwind min(yds)-0.06*diff(yext) max(yds)+0.06*diff(yext) ])
@@ -207,6 +286,72 @@ while 1 %evinfo.norids % loop on orids
             hp(1) = plot(ax1,[x1,x1],axlim(ax1,[3,4]),'--r','Linewidth',1.5);
             hp(2) = plot(ax1,[x2,x2],axlim(ax1,[3,4]),'--r','Linewidth',1.5);
         end
+
+        % calculate some things re the filter
+        centfreq = (filtfs(1)+filtfs(2))/2;
+        centperiod=1/centfreq;
+        if autosnwin
+            nwinlim = [-3*centperiod , -1*centperiod];
+            swinlim = [-1*centperiod , 1.5*centperiod];
+        end
+
+
+        %% SNR calculation and plot
+        % if that flag is on
+        % windowing 2.5 periods around the signal
+        if SNRdisp == 1
+            SNR = nan(size(all_datf,2),1);
+            noisewin = find(nwinlim(1) < tts & tts <  nwinlim(2)); 
+            sigwin   = find(swinlim(1) < tts & tts <  swinlim(2)); 
+
+            % loop through good stations, calc SNR
+            for ig = 1:length(indgd)
+                is = indgd(ig);
+               
+%                 SNR(is) = (rms(all_dastf(sigwin,is))/rms(all_datf(noisewin,is)))^2;  
+                SNR(is) = abs(maxab(all_datf(sigwin,is)))*rms(all_datf(sigwin,is))...
+                          /...
+                          rms(all_datf(noisewin,is))^2;  
+            
+                % plot the value SNR 
+                text(ax1,prelimwind(1)+2,yds(ig),['SNR: ' num2str(SNR(is))],...
+                    'fontsize',16,'VerticalAlignment','middle')
+
+                colr = colour_get(SNR(is),max(SNR),min(SNR),redblue);
+            end
+            % now colour the traces on the plot accordingly
+            for ig = 1:length(indgd)
+                is = indgd(ig);
+                colr = colour_get(log(SNR(is)),log(max(SNR)),log(min(SNR)),flipud(0.9*redblue).^2.5);
+%                 colr = colour_get(SNR(is),max(SNR),min(SNR),flipud(0.9*jet).^3);
+                set(htr(ig),'color',colr)
+            end
+            % plot histgram of the SNRs
+            figsnr = figure(98);clf(figsnr),set(figsnr,'pos',[42 867 614 405])
+            SNRbins = [0,0.5,0.8,1,1.5,2,3,4,5,10,20,30,40,50,75,100,150,200];
+            hist(SNR,SNRbins),
+            set(gca,'xscale','log','xlim',[0 200],'linewidth',1.5,'box','on','FontSize',15,'xtick',SNRbins)
+            xlabel('SNR','FontSize',17,'FontWeight','bold')
+            ylabel('counts','FontSize',17,'FontWeight','bold')
+
+            % back to reality.
+            figure(figxc);
+        end
+
+        %% add cheat sheet phases, if warranted
+        if phasedisp==1 && ydopt==1
+            % plot phase times, having subtracted off target time
+            plot(ax1,phasedb.dphasetimes,phasedb.gcdists,':r','linewidth',1.2);
+            % name the phases
+            for ip = 1:phasedb.Nph
+                gcplt = axlim(ax1,[3 4])*[0.98 0.02]'; % 1/25 of way up the plot
+                ttplt = interp1(phasedb.gcdists,phasedb.dphasetimes(:,ip),gcplt);
+                text(ax1,ttplt+1,gcplt,phasedb.phases{ip},'color','r','fontsize',15)
+            end
+        end
+        
+
+
 
         %% Option to edit filter
         % work on figure 
@@ -287,6 +432,45 @@ while 1 %evinfo.norids % loop on orids
 
                 ydopt = mod(ydopt+1,2);
 
+            case 's'  % toggle on/off display of estimated SNR
+                
+                SNRdisp = ~SNRdisp;
+
+            case 'v' % make stacked spectogram + other diagnostics  
+                
+                Make_Diagnostic_Figure_ze    
+                figure(figxc);
+
+            case 'c'  % toggle on/off display of cheat-sheet phases
+                
+                phasedisp = ~phasedisp;
+
+            case 'q' % quality control by SNR - choose SNR cutoff
+
+                snrqc = inputdlg({'Enter minimum SNR to keep'},'SNR-to-keep',1);
+                if isempty(snrqc{:}), continue; end
+                indgd( SNR(indgd) < eval(snrqc{:}) ) = []; % kill SNRs lower than cutoff
+
+            case 'w' % choose signal and noise windows
+                
+                if autosnwin % if was automatic, now manual
+                    title('FIRST CHOOSE SIGNAL WINDOW')
+                    % choose signal window
+                    [xs1,~] = ginput(1);
+                    hsw(1) = plot(ax1,[xs1,xs1],axlim(ax1,[3,4]),'--b','Linewidth',1.5);
+                    [xs2,~] = ginput(1);
+                    hsw(2) = plot(ax1,[xs2,xs2],axlim(ax1,[3,4]),'--b','Linewidth',1.5);
+                    swinlim = sort(round_level([xs1;xs2],1./resamprate));
+                    title('NOW CHOOSE START OF NOISE WINDOW')
+                     % choose noise window
+                    [xn1,~] = ginput(1);
+                    hnw(1) = plot(ax1,[xn1,xn1],axlim(ax1,[3,4]),'--r','Linewidth',1.5);
+                    nwinlim = sort(round_level([xn1;swinlim(1)],1./resamprate));
+                end
+                % flip from auto to manual
+                autosnwin = ~autosnwin;
+
+                
             case 'd'  % delete station with trace beneath cursor
 
                 indgd(mindex(yds,y)) = [];
@@ -388,7 +572,7 @@ while 1 %evinfo.norids % loop on orids
     try cla(ax3); end
     ax3 = axes(figxc,'pos',[0.68 0.07 0.27 0.85]); cla(ax3)
     set(ax3,'ytick',[],'box','on','fontsize',16,...
-        'xlim',[ -10 30]); 
+        'xlim',conflimwind); 
     hold on
     normf = max(max(abs(all_datwf(:,indgd))));
 %     normfx = max(max(abs(xcor_datwf(:,indgd))));
